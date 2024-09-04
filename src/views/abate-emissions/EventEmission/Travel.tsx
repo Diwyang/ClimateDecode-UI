3f38769340e8ce1570b220e413fc5cf81dc0f89d
Amit Bhatt <amitbhtt58@gmail.com>
2024-09-04 15:46:12 +0530

    fixes

--- src/views/abate-emissions/EventEmission/Travel.tsx
+++ src/views/abate-emissions/EventEmission/Travel.tsx
@@ -1,37 +1,37 @@
 import { Column } from '@ant-design/plots';
 import { Col, Row } from 'antd';
 
-const data: DataType[] = [
+const data: any[] = [
   {
     key: '2',
     category: 'Event',
-    tCO2Eq: 25,
-    totalPercentage: 25,
+    tCO2Eq: 90,
+    type: 'No of nights per person',
     color: '#15498E',
   },
   {
     key: '3',
     category: 'Materials',
-    tCO2Eq: 24,
-    totalPercentage: 24,
+    tCO2Eq: 44,
+    type: 'No of person who need accommodation',
     color: '#7E56ED',
   },
   {
     key: '4',
     category: 'Venue',
-    tCO2Eq: 24,
-    totalPercentage: 24,
+    tCO2Eq: 64,
+    type: 'Type of accommodation',
     color: '#EF912E',
   },
 ];
 
 const config = {
   data,
-  xField: 'totalPercentage',
+  xField: 'type',
   yField: 'tCO2Eq',
   style: {
-    fill: ({ totalPercentage }) => {
-      if (totalPercentage > 20) {
+    fill: ({ type }) => {
+      if (type > 20) {
         return '#9FC5E9';
       }
       return '#9FC5E9';
@@ -52,22 +52,7 @@ const config = {
 
 const createChart = () => (
   <Row style={{ minWidth: '400px' }} gutter={24}>
-    {data.map((chart) => (
-      <Col span={3} key={chart.key}>
-        <Column
-          {...config}
-          colors={[chart.color, '#9FC5E9']}
-          //   data={[
-          //     chart,
-          //     {
-          //       category: 'Others',
-          //       tCO2Eq: 100 - chart.tCO2Eq,
-          //       color: '#C8CCCD',
-          //     },
-          //   ]}
-        />
-      </Col>
-    ))}
+    <Column {...config} />
   </Row>
 );
 
