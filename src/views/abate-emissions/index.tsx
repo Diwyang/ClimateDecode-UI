8f53de79d836e6407c46daf4e44443bd1549c3cf
Amit Bhatt <52137465+amitbhatt007@users.noreply.github.com>
2024-12-10 08:43:42 +0530

    Update index.tsx

--- src/views/abate-emissions/index.tsx
+++ src/views/abate-emissions/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import LayoutPage from '../layout/Layout';
 import { Card, Col, Row } from 'antd';
 import '../data-collection/form/venue/tabs.scss';
@@ -9,29 +10,39 @@ import EventEmission from './EventEmission';
 import './abateEmission.scss';
 
 const AbateEmissions: React.FC = () => {
+  const checkLastPath = () => {
+    const location = useLocation();
+    const previousPathRef = useRef(location.pathname);
+  }
   return (
     <LayoutPage>
-      <EventEmission />
-      <h1>ABATE YOUR EVENT EMISSIONS</h1>
-      <Row gutter={24}>
-        <Col span={8}>
-          <Card title={<h3>Statistics</h3>}>
-            <Statistics />
-          </Card>
-        </Col>
-        <Col span={15}>
-          <Card title={<h3>Statistics</h3>}>
-            <Analysis />
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Card title={<h3>Top Emission Chart</h3>}>
-            <TopEmissionChart />
-          </Card>
-        </Col>
-      </Row>
+      {
+        checkLastPath() ?
+          <EventEmission />
+          :
+          <>
+            <h1>ABATE YOUR EVENT EMISSIONS</h1>
+            <Row gutter={24}>
+              <Col span={8}>
+                <Card title={<h3>Statistics</h3>}>
+                  <Statistics />
+                </Card>
+              </Col>
+              <Col span={15}>
+                <Card title={<h3>Statistics</h3>}>
+                  <Analysis />
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Card title={<h3>Top Emission Chart</h3>}>
+                  <TopEmissionChart />
+                </Card>
+              </Col>
+            </Row>
+          </>
+      }
     </LayoutPage>
   );
 };
