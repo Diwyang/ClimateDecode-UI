e02e84908994895ca5b031d50596d6efa7b9b9f4
Amit Bhatt <52137465+amitbhatt007@users.noreply.github.com>
2025-02-22 09:54:22 +0530

    Update apiSlice.ts

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -5,12 +5,12 @@ const baseQuery = fetchBaseQuery({
   //baseUrl: 'http://localhost:9091',
   baseUrl: 'http://20.197.13.9/api',
   // crendentials: "include" will face CORS if credential is not provided
-  credentials: 'same-origin',
+  //credentials: 'same-origin',
   prepareHeaders(headers, store: any) {
     const token = store.getState().auth.token;
     // headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
     // headers.append('Access-Control-Allow-Credentials', 'true');
-    headers.set('Access-Control-Allow-Origin', 'http://20.197.13.9/api');
+    //headers.set('Access-Control-Allow-Origin', 'http://20.197.13.9/api');
     if (token) {
      // headers.append('authorization', `Bearer ${token}`);
     }
